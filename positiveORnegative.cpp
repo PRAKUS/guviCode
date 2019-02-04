4c055a862389a974f771d9c69dc364ce4b9b5c4c
PRAKUS <pramodthapa62@gmail.com>
2019-02-04 21:21:08 +0530

    Create positiveORnegative.cpp

---
+++ positiveORnegative.cpp
@@ -0,0 +1,22 @@
+#include <iostream.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{	
+	int a;
+	cout<<Enter your number
+	cin>>a;
+	if (a>0)
+	{
+		cout<<"Positive";
+	}
+	else if (a<0)
+
+	{
+		cout<<"Negative";
+	}
+	else
+	{
+		cout<<"Zero"
+	}
+	return 0;
+}
