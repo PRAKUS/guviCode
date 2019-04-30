85c755019be57cdfbe3b8ae36216352f442f7b42
Pramod <pramodthapa62@gmail.com>
2019-04-30 21:59:34 +0530

    Create HunterSet1_Q1.cpp
    Given a number N followed by N numbers. Out of these N numbers some of them are repeated. Write a program to find the number which is repeated and print the repeated numbers in sorted order. If no numbers are repeated print "unique".
    Input Size : 1 <= N <= 100000

---
+++ HunterSet1_Q1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string input;
+    char data[input.length()];
+    for (int i=0;i<=input.length();i++)
+    {
+        data[i]=input[i];
+        //cout<<data[i];
+        if(i>1)
+        {
+            if(data[i-1]==data[i])
+            {
+                cout<<data[i];
+            }
+            
+        }
+        
+    }
+    
+}
